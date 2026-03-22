b4dcf5eedff6419b7081327d0a052bd2ea7fa7da
devopslabshandson-rgb <devopslabshandson@gmail.com>
2026-03-22 17:51:09 +0530

    Delete scale.py

--- scale.py
+++
@@ -1,9 +0,0 @@
-import sys
-import subprocess
-
-action = sys.argv[1]
-
-if action == "up":
-    subprocess.run(["kubectl", "scale", "deployment", "my-app", "--replicas=5"])
-elif action == "down":
-    subprocess.run(["kubectl", "scale", "deployment", "my-app", "--replicas=2"])
